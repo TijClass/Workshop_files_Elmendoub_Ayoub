f1fc6fa008bf318857b0782dac86dbf09ad98819
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-02-22 12:49:05 +0100

    fixing ui

---
+++ Workshop1/surfaceCalculator.php
@@ -0,0 +1,33 @@
+<?php
+
+$result = 'jjjj';
+
+echo $result;
+
+pi*r^2
+
+h*b/2
+
+A
+=
+a
++
+b
+/
+2
+*
+h
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>surface Calculator</title>
+</head>
+<body>
+    <div class="container"></div>
+</body>
+</html>
